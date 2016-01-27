4b58427bc307a66d3317fb4ae3b7e7682d50f610
Klabbedi <Klabbedi@users.noreply.github.com>
2016-01-27 20:44:37 +0100

    Update Line_follow.py
    Cosmetic changes to explaining text etc.

--- Line_follow.py
+++ Line_follow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
-from time   import sleep
+from time import sleep
 
 from ev3dev.auto import *
 
@@ -15,29 +15,28 @@ ki = float(0.02)
 direction = -1
 minRef = 44
 maxRef = 60
-
 # -------------------
 
-# Connect two large motors on output ports B and C:
-# motors = [LargeMotor(address) for address in (OUTPUT_B, OUTPUT_C)]
+# Connect two large motors on output ports B and C and check that
+# the device is connected using the 'connected' property.
 print 'Connecting motors'
 left_motor = LargeMotor(OUTPUT_B);  assert left_motor.connected
 right_motor = LargeMotor(OUTPUT_C); assert right_motor.connected
+# One left and one right motor should be connected
 
-# Every device in ev3dev has `connected` property. Use it to check that the
-# device has actually been connected.
-#assert all([m.connected for m in motors]), \
-#    "Two large motors should be connected to ports B and C"
-
-# Connect infrared and touch sensors and color
+# Connect infrared, color and touch sensors and check that they
+# are connected.
 print 'Connecting sensors'
-ir = InfraredSensor(); assert ir.connected
-ts = TouchSensor();    assert ts.connected
-col= ColorSensor();		 assert col.connected
+ir = InfraredSensor(); 	assert ir.connected
+ts = TouchSensor();    	assert ts.connected
+col= ColorSensor(); 	assert col.connected
 
+# Change color sensor mode
 print 'Setting color sensor mode'
 col.mode = 'COL-REFLECT'
 
+# Adding button so it would be possible to break the loop using
+# one of the buttons on the brick
 print 'Adding button'
 btn = Button()
 
@@ -56,7 +55,7 @@ def steering(course, power):
 	Output:
 		a tuple of power values for a pair of motors.
 	Example:
-		for (motor, power) in zip((left_motor, right_motor), steering(50, 900)):
+		for (motor, power) in zip((left_motor, right_motor), steering(50, 90)):
 			motor.run_forever(speed_sp=power)
 	"""
 
@@ -72,12 +71,13 @@ def steering(course, power):
 	return (int(pl), int(pr))
 
 def run(power, target, kp, kd, ki, direction, minRef, maxRef):
+	"""
+	Description...
+	"""
 	print 'Run module..'
 	lastError = 0
 	error = 0
 	integral = 0
-#	left_motor.run_direct(duty_cycle_sp = power)
-#	right_motor.run_direct(duty_cycle_sp = power)
 	left_motor.run_direct()
 	right_motor.run_direct()
 	lap = 1
@@ -102,8 +102,6 @@ def run(power, target, kp, kd, ki, direction, minRef, maxRef):
 		for (motor, pow) in zip((left_motor, right_motor), steering(course, power)):
 			motor.duty_cycle_sp = pow
 			print 'Pow: ' + str(pow)
-#			motor.run_direct(duty_cycle_sp = pow)
-#			motor.run_forever(speed_sp = pow)
 		lap = lap + 1
 		print '-----'
 		sleep(1)
