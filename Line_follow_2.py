6663f17e448acf150c214186936ad8417a3ac868
Klabbedi <Klabbedi@users.noreply.github.com>
2016-01-29 21:06:24 +0100

    Create Line_follow_2.py
    New file

---
+++ Line_follow_2.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from time import sleep
+
+from ev3dev.auto import *
+
+# ------Input--------
+print 'Setting input values'
+power = 60
+target = 55
+kp = float(0.65) # Start value 1
+kd = 1           # Start value 0
+ki = float(0.02) # Start value 0
+direction = -1
+minRef = 41
+maxRef = 63
+# -------------------
+
+# Connect two large motors on output ports B and C and check that
+# the device is connected using the 'connected' property.
+print 'Connecting motors'
+left_motor = LargeMotor(OUTPUT_B);  assert left_motor.connected
+right_motor = LargeMotor(OUTPUT_C); assert right_motor.connected
+# One left and one right motor should be connected
+
+# Connect infrared, color and touch sensors and check that they
+# are connected.
+print 'Connecting sensors'
+ir = InfraredSensor(); 	assert ir.connected
+ts = TouchSensor();    	assert ts.connected
+col= ColorSensor(); 	assert col.connected
+
+# Change color sensor mode
+print 'Setting color sensor mode'
+col.mode = 'COL-REFLECT'
+
+# Adding button so it would be possible to break the loop using
+# one of the buttons on the brick
+print 'Adding button'
+btn = Button()
+
+def steering(course, power):
+	"""
+	Computes how fast each motor in a pair should turn to achieve the
+	specified steering.
+	Input:
+		direction [-100, 100]:
+		* -100 means turn left as fast as possible,
+		*  0   means drive in a straight line, and
+		*  100 means turn right as fast as possible.
+	power: the power that should be applied to the outmost motor (the one
+		rotating faster). The power of the other motor will be computed
+		automatically.
+	Output:
+		a tuple of power values for a pair of motors.
+	Example:
+		for (motor, power) in zip((left_motor, right_motor), steering(50, 90)):
+			motor.run_forever(speed_sp=power)
+	"""
+
+	pl = power
+	pr = power
+	s = (50 - abs(float(course))) / 50
+
+	if course >= 0:
+		pr *= s
+		if course > 100:
+			pr = - power
+	else:
+		pl *= s
+		if course < -100:
+			pl = - power
+
+	return (int(pl), int(pr))
+	
+def steering2(course, power):
+	if course >= 0:
+		if course > 100:
+			pr = 0
+			pl = power
+		else:	
+			pl = power
+			pr = power - ((power * course) / 100)
+	else:
+		if course < -100:
+			pl = 0
+			pr = power
+		else:
+			pr = power
+			pl = power + ((power * course) / 100)
+	return (int(pl), int(pr))
+
+def run(power, target, kp, kd, ki, direction, minRef, maxRef):
+	"""
+	Description...
+	"""
+	print 'Run module..'
+	lastError = 0
+	error = 0
+	integral = 0
+	left_motor.run_direct()
+	right_motor.run_direct()
+	lap = 1
+	while not btn.any() :
+#		print "Lap: " + str(lap)
+		if ts.value():
+			print 'Breaking loop'
+			break
+		refRead = col.value()
+#		print 'Reflective value: ' + str(refRead)
+		error = target - (100 * ( refRead - minRef ) / ( maxRef - minRef ))
+#		print 'Error: ' + str(error)
+		derivative = error - lastError
+#		print 'Derivative: ' + str(derivative)
+		lastError = error
+#		print 'Last error: ' + str(lastError)
+		integral = float(0.5) * integral + error
+#		print 'Integral: ' + str(integral)
+		course = (kp * error + kd * derivative +ki * integral) * direction
+#		print 'Course: ' + str(course)
+#		print 'Steering: ' + str(steering(course,power))
+		for (motor, pow) in zip((left_motor, right_motor), steering(course, power)):
+			motor.duty_cycle_sp = pow
+#			print 'Pow: ' + str(pow)
+		lap = lap + 1
+#		print '-----'
+		sleep(0.01)
+
+run(power, target, kp, kd, ki, direction, minRef, maxRef)
+
+# Stop the motors before exiting.
+print 'Stopping motors'
+left_motor.stop()
+right_motor.stop()
