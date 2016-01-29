91344f39990635f4a7571bea821b71da08b13214
Klabbedi <Klabbedi@users.noreply.github.com>
2016-01-29 13:04:56 +0100

    Create max_min_finder.py
    New max, min finder program

---
+++ max_min_finder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from time   import sleep
+
+from ev3dev.auto import *
+
+left_motor = LargeMotor(OUTPUT_B);  assert left_motor.connected
+right_motor = LargeMotor(OUTPUT_C); assert right_motor.connected
+col= ColorSensor();		 assert col.connected
+col.mode = 'COL-REFLECT'
+
+def run():left_motor.run_timed(duty_cycle_sp=10, time_sp=4000)
