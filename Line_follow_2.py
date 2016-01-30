17af1f943b90772288d8f66c8910489ef79fc497
Klabbedi <Klabbedi@users.noreply.github.com>
2016-01-30 16:32:14 +0100

    Update Line_follow_2.py
    Minor edits

--- Line_follow_2.py
+++ Line_follow_2.py
@@ -9,9 +9,9 @@ from ev3dev.auto import *
 print 'Setting input values'
 power = 60
 target = 55
-kp = float(0.65) # Start value 1
-kd = 1           # Start value 0
-ki = float(0.02) # Start value 0
+kp = float(0.65) # Proportional gain. Start value 1
+kd = 1           # Derivative gain. Start value 0
+ki = float(0.02) # Integral gain. Start value 0
 direction = -1
 minRef = 41
 maxRef = 63
@@ -41,21 +41,40 @@ print 'Adding button'
 btn = Button()
 
 def steering2(course, power):
+	"""
+	Computes how fast each motor in a pair should turn to achieve the
+	specified steering.
+	Input:
+		course [-100, 100]:
+		* -100 means turn left as fast as possible,
+		*  0   means drive in a straight line, and
+		*  100  means turn right as fast as possible.
+		* If >100 power_right = -power
+		* If <100 power_left = power        
+	power: the power that should be applied to the outmost motor (the one
+		rotating faster). The power of the other motor will be computed
+		automatically.
+	Output:
+		a tuple of power values for a pair of motors.
+	Example:
+		for (motor, power) in zip((left_motor, right_motor), steering(50, 90)):
+			motor.run_forever(speed_sp=power)
+	"""
 	if course >= 0:
 		if course > 100:
-			pr = 0
-			pl = power
+			power_right = 0
+			power_left = power
 		else:	
-			pl = power
-			pr = power - ((power * course) / 100)
+			power_left = power
+			power_right = power - ((power * course) / 100)
 	else:
 		if course < -100:
-			pl = 0
-			pr = power
+			power_left = 0
+			power_right = power
 		else:
-			pr = power
-			pl = power + ((power * course) / 100)
-	return (int(pl), int(pr))
+			power_right = power
+			power_left = power + ((power * course) / 100)
+	return (int(power_left), int(power_right))
 
 def run(power, target, kp, kd, ki, direction, minRef, maxRef):
 	"""
@@ -70,15 +89,12 @@ def run(power, target, kp, kd, ki, direction, minRef, maxRef):
 		minRef. Min reflecting value of floor or line
 		maxRef. Max reflecting value of floor or line 
 	"""
-	lastError = 0
-	error = 0
-	integral = 0
+	lastError = error = integral = 0
 	left_motor.run_direct()
 	right_motor.run_direct()
-	lap = 1
 	while not btn.any() :
 		if ts.value():
-			print 'Breaking loop'
+			print 'Breaking loop' # User pressed touch sensor
 			break
 		refRead = col.value()
 		error = target - (100 * ( refRead - minRef ) / ( maxRef - minRef ))
@@ -88,8 +104,7 @@ def run(power, target, kp, kd, ki, direction, minRef, maxRef):
 		course = (kp * error + kd * derivative +ki * integral) * direction
 		for (motor, pow) in zip((left_motor, right_motor), steering2(course, power)):
 			motor.duty_cycle_sp = pow
-		lap = lap + 1
-		sleep(0.01)
+		sleep(0.01) # Aprox 100Hz
 
 run(power, target, kp, kd, ki, direction, minRef, maxRef)
 
